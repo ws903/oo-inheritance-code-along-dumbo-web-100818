4604b931599c8a4cc0b813a0affa4aa778dfdfeb
David Sim <wsim903@gmail.com>
2018-10-12 04:25:38 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,9 @@
 require_relative "./vehicle.rb"
+
+class Car
+  @attr_reader(:wheel_size, :number)
+  def initialize(wheel_size, number)
+    @wheel_size = wheel_size
+    @number = number
+  end
+end
